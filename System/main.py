b5d0962abc4d714e474eb888b5bf5e4fe8778605
Jake <valaniftbuisness@gmail.com>
2025-11-10 23:32:28 -0800

    Added database
    Lots of visual modifications but I added the database and had everything render with it.

--- System/main.py
+++ System/main.py
@@ -1,19 +1,48 @@
 from flask import *
 from markupsafe import *
+from pymongo import *
+from bson.objectid import ObjectId
+import random
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+client = MongoClient(os.getenv("DATABASE_URL"))
+db = client[os.getenv("DATABASE_NAME")]
 
-@app.route("/<int:movieid>", methods=["GET"])
+@app.route("/<movieid>", methods=["GET"])
 def movie_info(movieid):
-    name = "Scary House"
-    img_path = url_for('static', filename='uiconcept.png')
-    description = "it's scary"
-    rating = "5" + "/5"
-    return render_template('movie.html', title = name, description = description, rating = rating, img_path = img_path)
+    s_movie = db.Movies.find_one({"_id": ObjectId(str(movieid))})
+    name = s_movie["Name"]
+    if ":" in s_movie["Name"]:
+        name = s_movie["Name"].replace(":", " -")
+    img_path = url_for('static', filename=name + ".webp")
+    cast = ", ".join(s_movie["Cast"])
+    director = s_movie["Director"]
+    genre = ", ".join(s_movie["Genres"])
+    runtime = s_movie["Runtime"]
+    rating = s_movie["Rating"]
+    release_date = s_movie["release_date"]
+    return render_template('movie.html', name=name, img_path=img_path, rating=rating, release_date=release_date, director=director, cast=cast, genre=genre, runtime=runtime)
+
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template('home.html')
+    featured_movies = list(db.Movies.aggregate([{"$sample": {"size": 8}}]))
+    return render_template('home.html', featured_movies=featured_movies)
+
+@app.route("/", methods=["POST"])
+def home_search():
+    q = request.form.get("query")
+    query = db.Movies.find({}, {"Name": {"$regex": q, "$options": "i"}})
+    objID = query[0]['_id'].__str__()
+    print(query)
+    print(objID)
+    return redirect(url_for('movie_info', movieid=objID))
+    
+    
 
 @app.route("/home", methods=["GET"])
 def home_redirect():
@@ -29,4 +58,11 @@ def contact():
 
 @app.route("/list", methods=["GET"])
 def list_movies():
-    return render_template('list.html')
+    db_movies = db.Movies.find()
+    movies = []
+    for movie in db_movies:
+        movies.append(movie)
+    return render_template('list.html', movies=movies)
+
+if __name__ == "__main__":
+    app.run(debug=True)
